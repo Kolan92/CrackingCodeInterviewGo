2d4df50c7c894a73b18ac4355a9a4201b431f94f
Paweł Kolanowski <pawel.krzysztof.kolanowski@gmail.com>
2021-12-29 17:15:57 +0100

    Add array and strings solutions

---
+++ 1_array_and_strings.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"strings"
+	"fmt"
+	"errors"
+)
+
+func hasOnlyUniqueChars(input string) bool {
+	for i := 0; i < len(input); i++ {
+		for j := i + 1; j < len(input); j++ {
+			if input[i] == input[j] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func reverse(input string) string {
+	inputLength := len(input)
+	if inputLength == 0 {
+		return input
+	}
+
+	reversedArray := make([]rune, inputLength)
+	for index, char := range input {
+		reversedArray[inputLength-index-1] = char
+
+	}
+
+	return string(reversedArray)
+}
+
+func isPermutation(input, other string) bool {
+	return isPermutationOf[rune]([]rune(input), []rune(other))
+}
+
+func isPermutationOf[K comparable](input, other []K) bool {
+	existingChars := map[K]bool {}
+	for _, char := range input {
+		existingChars[char] = true
+	}
+
+	for _, char := range other {
+		if _, hasKey := existingChars[char]; !hasKey {
+			return false
+		}
+	}
+
+	return true
+}
+
+func encodeSpaces(input string, length int) string {
+	if length < 0 {
+		length = 0
+	}
+
+	runes := []rune(input)[:length]
+	output := make([]string, length)
+
+	for index, char := range runes {
+		if char == ' ' {
+			output[index] = "%20"
+		} else {
+			output[index] = string(char)
+		}
+	}
+
+	return strings.Join(output, "")
+}
+
+type compressEntry struct {
+	count int
+	char  rune
+}
+
+func (entry compressEntry) toString() string {
+	return fmt.Sprintf("%s%d", string(entry.char), entry.count)
+}
+
+func compres(input string) string {
+	inputLength := len(input)
+	if inputLength == 0 {
+		return input
+	}
+
+	entries := []compressEntry{}
+	runes := []rune(input)
+	currentEntry := compressEntry {
+		char:  runes[0],
+		count: 0,
+	}
+
+	for _, char := range runes {
+		if currentEntry.char == char {
+			currentEntry.count++
+		} else {
+			entries = append(entries, currentEntry)
+			currentEntry = compressEntry {
+				char:  char,
+				count: 1,
+			}
+		}
+	}
+	entries = append(entries, currentEntry)
+
+	convertedEntries, _ := Map(entries, func(entry compressEntry) string {
+		return entry.toString()
+	})
+
+	compressedString := strings.Join(convertedEntries, "")
+	if inputLength > len(compressedString) {
+		return compressedString
+	} else {
+		return input
+	}
+}
+
+func Map[KInput any, KOutput any](input []KInput, transform func(KInput) KOutput) ([]KOutput, error) {
+	if input == nil {
+		return []KOutput {}, errors.New("Nil input")
+	}
+
+	if transform == nil {
+		return []KOutput {}, errors.New("Nil transform")
+	}
+	output := make([]KOutput, len(input))
+
+	for index, value := range input {
+		output[index] = transform(value)
+	}
+
+	return output, nil
+}
+
+
+type pixel struct {
+	red, green,	blue, alpha byte
+}
+
+func rotateMatrix90Degrees(matrix *[][]pixel) {
+	size := len(*matrix)
+	halfSize := size / 2
+	for layer := 0; layer < halfSize; layer++ {
+		first := layer
+		last := size - 1 - layer
+		for index := first; index < last; index++ {
+			elementIndex := last - index - first
+			topElement := (*matrix)[first][index]
+
+			//left -> top
+			(*matrix)[first][index] = (*matrix)[elementIndex][first]
+			//bottom -> left
+			(*matrix)[elementIndex][first] = (*matrix)[last][elementIndex]
+			//right -> bottom
+			(*matrix)[last][elementIndex] = (*matrix)[index][last]
+			//top -> right
+			(*matrix)[index][last] = topElement
+		}
+	}
+}
+
+
+func zeroColumnsAndRows(matrix *[][]int) {
+	zeroRows := make(map[int]bool)
+	zeroColumns := make(map[int]bool)
+
+	size := len(*matrix)
+
+	for row := 0; row < size; row++ {
+		for column := 0; column < size; column++ {
+			if (*matrix)[row][column] == 0 {
+				zeroRows[row] = true
+				zeroColumns[column] = true
+			}
+		}
+	}
+
+	for row := 0; row < size; row++ {
+		_, isZeroRow := zeroRows[row]
+
+		for column := 0; column < size; column++ {
+			_, isZeroColumn := zeroColumns[column]
+			if isZeroRow || isZeroColumn {
+				(*matrix)[row][column] = 0
+			}
+		}
+	}
+}
+
+func isRotated(source, candidate string) bool {
+	if len(source) != len(candidate) {
+		return false
+	}
+	return strings.Contains(source + source, candidate)
+}
---
+++ 1_array_and_strings_test.go
@@ -0,0 +1,433 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"strings"
+)
+
+func TestHasOnlyUniqueChars(t *testing.T) {
+	t.Run("Empty string", func(t *testing.T) {
+		hasOnlyUniqueChars := hasOnlyUniqueChars("")
+		assert.True(t, hasOnlyUniqueChars)
+	})
+
+	t.Run("One letter string", func(t *testing.T) {
+		hasOnlyUniqueChars := hasOnlyUniqueChars("a")
+		assert.True(t, hasOnlyUniqueChars)
+	})
+
+	t.Run("Two same letters string", func(t *testing.T) {
+		hasOnlyUniqueChars := hasOnlyUniqueChars("dd")
+		assert.False(t, hasOnlyUniqueChars)
+	})
+
+	t.Run("Upper and lower case letters", func(t *testing.T) {
+		hasOnlyUniqueChars := hasOnlyUniqueChars("Dd")
+		assert.True(t, hasOnlyUniqueChars)
+	})
+
+	t.Run("Longger unique string", func(t *testing.T) {
+		hasOnlyUniqueChars := hasOnlyUniqueChars("DawIoqd")
+		assert.True(t, hasOnlyUniqueChars)
+	})
+
+	t.Run("Longger not unique string", func(t *testing.T) {
+		hasOnlyUniqueChars := hasOnlyUniqueChars("DawIoqdD")
+		assert.False(t, hasOnlyUniqueChars)
+	})
+}
+
+func TestReverse(t *testing.T) {
+	t.Run("Empty string", func(t *testing.T) {
+		reversed := reverse("")
+		assert.Equal(t, "", reversed)
+	})
+
+	t.Run("Longer string", func(t *testing.T) {
+		reversed := reverse("tyDesR")
+		assert.Equal(t, "RseDyt", reversed)
+	})
+}
+
+func TestIsPermutation(t *testing.T) {
+	t.Run("Empty strings", func(t *testing.T) {
+		isPermutation := isPermutation("", "")
+		assert.True(t, isPermutation)
+	})
+
+	t.Run("Not permutated int inputs", func(t *testing.T)  {
+		isPermutation := isPermutationOf([]int{1, 2, 3}, []int{1, 2, 5} )
+		assert.False(t, isPermutation)
+	})
+
+	t.Run("Not permutated double inputs", func(t *testing.T)  {
+		isPermutation := isPermutationOf([]float32{1.5, 2.5, 3.5}, []float32{1.5, 2.5, 5.5} )
+		assert.False(t, isPermutation)
+	})
+
+	t.Run("Not permutated when some letters are not the same case", func(t *testing.T)  {
+		isPermutation := isPermutation("Axl Rose", "Oral Sex")
+		assert.False(t, isPermutation)
+	})
+
+	t.Run("Permutated string inputs", func(t *testing.T)  {
+		isPermutation := isPermutation(strings.ToLower("Axl Rose"), strings.ToLower("Oral Sex"))
+		assert.True(t, isPermutation)
+	})
+
+	t.Run("Permutated double inputs", func(t *testing.T)  {
+		isPermutation := isPermutationOf([]float32{1.5, 2.5, 3.5}, []float32{1.5, 2.5, 3.5} )
+		assert.True(t, isPermutation)
+	})
+}
+
+func TestEncodeString(t *testing.T) {
+	t.Run("Empty strings", func(t *testing.T) {
+		encoded := encodeSpaces("", 0)
+		assert.Equal(t, "", encoded)
+	})
+
+	t.Run("Negative length", func(t *testing.T) {
+		encoded := encodeSpaces("", 0)
+		assert.Equal(t, "", encoded)
+	})
+
+	t.Run("Only spaces", func(t *testing.T) {
+		encoded := encodeSpaces("   ", 0)
+		assert.Equal(t, "", encoded)
+	})
+
+	t.Run("Longer input", func(t *testing.T) {
+		encoded := encodeSpaces("Axl Rose   ", 8)
+		assert.Equal(t, "Axl%20Rose", encoded)
+	})
+}
+
+func TestCompresString(t *testing.T) {
+	t.Run("Empty string", func(t *testing.T)  {
+		compressed := compres("")
+		assert.Equal(t, "", compressed)
+	})
+
+	t.Run("Should compres string", func(t *testing.T)  {
+		compressed := compres("aabcccccaaa")
+		assert.Equal(t, "a2b1c5a3", compressed)
+	})
+
+	t.Run("Should return original string when compressed is longer", func(t *testing.T) {
+		compressed := compres("abc")
+		assert.Equal(t, "abc", compressed)
+	})
+}
+
+func TestRotateMatrix90Degrees(t *testing.T) {
+	t.Run("Empty matrix", func(t *testing.T) {
+		matrix := [][]pixel{}
+		rotateMatrix90Degrees(&matrix)
+		assert.Equal(t, [][]pixel{}, matrix)
+	})
+
+	t.Run("One element matrix", func(t *testing.T) {
+		matrix := [][]pixel{ 
+			[]pixel{ 
+				pixel {
+					red: 1,
+					green: 1,
+					blue: 1,
+					alpha: 1,
+				},
+			},
+		}
+		expectedMatrix := [][]pixel{ 
+			[]pixel{ 
+				pixel {
+					red: 1,
+					green: 1,
+					blue: 1,
+					alpha: 1,
+				},
+			},
+		}
+		rotateMatrix90Degrees(&matrix)
+		assert.Equal(t, expectedMatrix, matrix)
+	})
+
+	t.Run("Four elements matrix", func(t *testing.T) {
+		matrix := [][]pixel{ 
+			[]pixel{ 
+				pixel {
+					red: 1,
+					green: 1,
+					blue: 1,
+					alpha: 1,
+				},
+				pixel {
+					red: 2,
+					green: 2,
+					blue: 2,
+					alpha: 2,
+				},
+			},
+			[]pixel{ 
+				pixel {
+					red: 3,
+					green: 3,
+					blue: 3,
+					alpha: 3,
+				},
+				pixel {
+					red: 4,
+					green: 4,
+					blue: 4,
+					alpha: 4,
+				},
+			},
+		}
+		rotateMatrix90Degrees(&matrix)
+
+		expectedMatrix := [][]pixel{ 
+			[]pixel{ 
+				pixel {
+					red: 3,
+					green: 3,
+					blue: 3,
+					alpha: 3,
+				},
+				pixel {
+					red: 1,
+					green: 1,
+					blue: 1,
+					alpha: 1,
+				},
+			},
+			[]pixel{ 
+				pixel {
+					red: 4,
+					green: 4,
+					blue: 4,
+					alpha: 4,
+				},
+				pixel {
+					red: 2,
+					green: 2,
+					blue: 2,
+					alpha: 2,
+				},
+			},
+		}
+		assert.Equal(t, expectedMatrix, matrix)
+	})
+
+	t.Run("Nine elements matrix", func(t *testing.T) {
+		matrix := [][]pixel{ 
+			[]pixel{ 
+				pixel {
+					red: 1,
+					green: 1,
+					blue: 1,
+					alpha: 1,
+				},
+				pixel {
+					red: 2,
+					green: 2,
+					blue: 2,
+					alpha: 2,
+				},
+				pixel {
+					red: 3,
+					green: 3,
+					blue: 3,
+					alpha: 3,
+				},
+			},
+			[]pixel{ 
+				pixel {
+					red: 4,
+					green: 4,
+					blue: 4,
+					alpha: 4,
+				},
+				pixel {
+					red: 5,
+					green: 5,
+					blue: 5,
+					alpha: 5,
+				},
+				pixel {
+					red: 6,
+					green: 6,
+					blue: 6,
+					alpha: 6,
+				},
+			},
+			[]pixel{ 
+				pixel {
+					red: 7,
+					green: 7,
+					blue: 7,
+					alpha: 7,
+				},
+				pixel {
+					red: 8,
+					green: 8,
+					blue: 8,
+					alpha: 8,
+				},
+				pixel {
+					red: 9,
+					green: 9,
+					blue: 9,
+					alpha: 9,
+				},
+			},
+		}
+		rotateMatrix90Degrees(&matrix)
+
+		expectedMatrix := [][]pixel{ 
+			[]pixel{ 
+				pixel {
+					red: 7,
+					green: 7,
+					blue: 7,
+					alpha: 7,
+				},
+				pixel {
+					red: 4,
+					green: 4,
+					blue: 4,
+					alpha: 4,
+				},
+				pixel {
+					red: 1,
+					green: 1,
+					blue: 1,
+					alpha: 1,
+				},
+			},
+			[]pixel{ 
+				pixel {
+					red: 8,
+					green: 8,
+					blue: 8,
+					alpha: 8,
+				},
+				pixel {
+					red: 5,
+					green: 5,
+					blue: 5,
+					alpha: 5,
+				},
+				pixel {
+					red: 2,
+					green: 2,
+					blue: 2,
+					alpha: 2,
+				},
+			},
+			[]pixel{ 
+				pixel {
+					red: 9,
+					green: 9,
+					blue: 9,
+					alpha: 9,
+				},
+				pixel {
+					red: 6,
+					green: 6,
+					blue: 6,
+					alpha: 6,
+				},
+				pixel {
+					red: 3,
+					green: 3,
+					blue: 3,
+					alpha: 3,
+				},
+			},
+		}
+
+		assert.Equal(t, expectedMatrix, matrix)
+	})
+}
+
+func TestZeroColumnsAndRows(t *testing.T) {
+	t.Run("Empty matrix", func(t *testing.T) {
+		matrix := [][]int{}
+		zeroColumnsAndRows(&matrix)
+		assert.Equal(t, [][]int{}, matrix)
+	})
+
+	t.Run("Matrix without zeros", func(t *testing.T) {
+		matrix := [][]int{
+			[]int { 2, 4, 6},
+			[]int { 1, 2, 3},
+			[]int { 7, 9, 8},
+		}
+		expectedMatrix := [][]int{
+			[]int { 2, 4, 6},
+			[]int { 1, 2, 3},
+			[]int { 7, 9, 8},
+		}
+
+		assert.Equal(t, expectedMatrix, matrix)
+	})
+
+	t.Run("Should insert zeroes in small matrix", func(t *testing.T) {
+		matrix := [][]int{
+			[]int { 0, 4},
+			[]int { 1, 2},
+		}
+		expectedMatrix := [][]int{
+			[]int { 0, 0},
+			[]int { 0, 2},
+		}
+		zeroColumnsAndRows(&matrix)
+
+		assert.Equal(t, expectedMatrix, matrix)
+	})
+
+	t.Run("Should insert zeroes in big matrix", func(t *testing.T) {
+		matrix := [][]int{
+			[]int { 0, 4, 6, 6, 7},
+			[]int { 1, 2, 3, 4, 6},
+			[]int { 7, 0, 4, 9, 8},
+			[]int { 1, 2, 5, 4, 6},
+			[]int { 7, 2, 3, 4, 6},
+
+		}
+		expectedMatrix := [][]int{
+			[]int { 0, 0, 0, 0, 0},
+			[]int { 0, 0, 3, 4, 6},
+			[]int { 0, 0, 0, 0, 0},
+			[]int { 0, 0, 5, 4, 6},
+			[]int { 0, 0, 3, 4, 6},
+		}
+		zeroColumnsAndRows(&matrix)
+
+		assert.Equal(t, expectedMatrix, matrix)
+	})
+}
+
+func TestIsRotated(t *testing.T) {
+	t.Run("Empty string", func(t *testing.T) {
+		isRotated := isRotated("", "")
+		assert.True(t, isRotated)
+	})
+
+	t.Run("Not rotated  string", func(t *testing.T) {
+		isRotated := isRotated("David", "Bowie")
+		assert.False(t, isRotated)
+	})
+
+	t.Run("Rotated string", func(t *testing.T) {
+		isRotated := isRotated("AxlRose", "lRoseAx")
+		assert.True(t, isRotated)
+	})
+
+	t.Run("Parial string", func(t *testing.T) {
+		isRotated := isRotated("David", "avi")
+		assert.False(t, isRotated)
+	})
+
+}
